1b82c5a4d0bd8603f2716157f018bb0fba70f655
Rémy <remycomeau0@gmail.com>
2024-10-16 16:56:57 +0200

    :bug: Removing the new arrayCollection from an event for the user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -64,7 +64,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     public function __construct()
     {
-        $this->events = new ArrayCollection();
         $this->createdAt = new \DateTimeImmutable();
         $this->updatedAt = new \DateTimeImmutable();
     }
